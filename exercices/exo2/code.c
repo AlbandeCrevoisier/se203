666de45433afb35bdcc6dbfacd9ddae24a154128
alban <decrevoisier@enst.fr>
2015-12-14 11:41:56 +0100

    Exercice 2.

--- exercices/exo2/code.c
+++ exercices/exo2/code.c
@@ -2,28 +2,21 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-int8_t a = 42;
-int16_t b = 42;
-int32_t c = 42;
-int64_t d = 42;
-int64_t e = 42;
-int32_t f = 42;
-int16_t g = 42;
-int8_t h = 42;
-int8_t foo;
-int16_t bar;
-int32_t baz;
-
-const char msg[] = "Hello world!\n";
+void
+f();
 
 int
 main()
 {
-	baz = 1337;
-	int x = 42;
-	printf("%d\n", x);
-	int *y = malloc(sizeof(int));
-	printf("%ldu\n", sizeof(y));
+    int64_t a = 42;
+    int64_t *b = malloc(sizeof(int64_t));
+    int64_t c = (int64_t)&f;
+    printf("stack: %p\n", (void*)&a);
+    printf("heap: %p\n", (void*)b);
+    printf("other sections: 0x%lx\n", c);
 
-	return 0;
+    return 0;
 }
+
+void
+f() {}
