0369e92b73757ae50e75b01612a201b0f1a4dd2f
ACH <albandecrevoisier@gmail.com>
2015-12-14 04:06:41 +0100

    Exercice 1.

---
+++ exercices/exo1/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int32_t x = 42;
+int32_t y = 42;
+int32_t z = 42;
+int8_t foo;
+int8_t bar;
+
+const char msg[] = "Hello world!\n";
+
+int
+main()
+{
+	y = 1337;
+
+	return 0;
+}
