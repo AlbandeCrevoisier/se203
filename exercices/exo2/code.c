52765dd19e91bea4af74beadb9cdf72d87d60c27
ACH <albandecrevoisier@gmail.com>
2015-12-14 15:14:03 +0100

    Removed unnecessary function, now use the address of main.

--- exercices/exo2/code.c
+++ exercices/exo2/code.c
@@ -2,15 +2,12 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-void
-f();
-
 int
 main()
 {
     int64_t a = 42;
     int64_t *b = malloc(sizeof(int64_t));
-    int64_t c = (int64_t)&f;
+    int64_t c = (int64_t)&main;
     printf("stack: %p\n", (void*)&a);
     printf("heap: %p\n", (void*)b);
     printf("other sections: 0x%lx\n", c);
@@ -18,5 +15,3 @@ main()
     return 0;
 }
 
-void
-f() {}
