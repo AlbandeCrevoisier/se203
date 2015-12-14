6381b43152fcecf1ac6ad905a053b7e3c577a3c6
ACH <albandecrevoisier@gmail.com>
2015-12-14 04:11:52 +0100

    Added a better code to illustrate exo1.

--- exercices/exo1/code.c
+++ exercices/exo1/code.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int32_t x = 42;
-int32_t y = 42;
-int32_t z = 42;
+int8_t a = 42;
+int16_t b = 42;
+int32_t c = 42;
+int64_t d = 42;
+int64_t e = 42;
+int32_t f = 42;
+int16_t g = 42;
+int8_t h = 42;
 int8_t foo;
-int8_t bar;
+int16_t bar;
+int32_t baz;
 
 const char msg[] = "Hello world!\n";
 
 int
 main()
 {
-	y = 1337;
+	baz = 1337;
 
 	return 0;
 }
