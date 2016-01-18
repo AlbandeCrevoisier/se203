57f9973cc70f752fd57071d8bb2be1f5cda1eb9f
alban <decrevoisier@enst.fr>
2016-01-18 11:31:23 +0100

    matrix_init() : WIP

--- td/matrix/matrix.c
+++ td/matrix/matrix.c
@@ -1,1 +1,49 @@
 /* Matrix */
+#include matrix.h
+
+void
+matrix_init(void)
+{
+	/* Clocks port A, B, C, and D */
+	SIM_SCGC5 |= (SET(9) + SET(10) + SET(11) + SET(12));
+
+	gpio_mode(SB);
+	gpio_mode(LAT);
+	gpio_mode(RST);
+	gpio_mode(SCK);
+	gpio_mode(SDA);
+	gpio_mode(C0);
+	gpio_mode(C1);
+	gpio_mode(C2);
+	gpio_mode(C3);
+	gpio_mode(C4);
+	gpio_mode(C5);
+	gpio_mode(C6);
+	gpio_mode(C7);
+
+	/* output mode */
+	GPIOB_PDDR |= SET(0);
+	GPIOB_PDDR |= SET(1);
+	GPIOB_PDDR |= SET(2);
+	GPIOC_PDDR |= SET(8);
+	GPIOC_PDDR |= SET(9);
+	GPIOA_PDDR |= SET(13);
+	GPIOD_PDDR |= SET(2);
+	GPIOD_PDDR |= SET(4);
+	GPIOD_PDDR |= SET(6);
+	GPIOD_PDDR |= SET(7);
+	GPIOD_PDDR |= SET(5);
+	GPIOA_PDDR |= SET(12);
+	GPIOA_PDDR |= SET(4);
+
+	/* init */
+	
+}
+
+void
+gpio_mode(*(volatile uint32_t *) p)
+{
+	p |= SET(8);
+	p &= CLEAR(9);
+	p &= CLEAR(10);
+}
--- td/matrix/matrix.h
+++ td/matrix/matrix.h
@@ -2,10 +2,57 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <stdint.h>
+
 #define SIM_SCGC5 (*(volatile uint32_t *) 0x40048038)
+#define GPIOA_PSOR (*(volatile uint32_t *) 0x400FF004)
+#define GPIOA_PCOR (*(volatile uint32_t *) 0x400FF008)
+#define GPIOA_PTOR (*(volatile uint32_t *) 0x400FF00C)
+#define GPIOA_PDDR (*(volatile uint32_t *) 0x400FF014)
+#define GPIOB_PSOR (*(volatile uint32_t *) 0x400FF044)
+#define GPIOB_PCOR (*(volatile uint32_t *) 0x400FF048)
+#define GPIOB_PTOR (*(volatile uint32_t *) 0x400FF04C)
+#define GPIOB_PDDR (*(volatile uint32_t *) 0x400FF054)
+#define GPIOC_PSOR (*(volatile uint32_t *) 0x400FF084)
+#define GPIOC_PCOR (*(volatile uint32_t *) 0x400FF088)
+#define GPIOC_PTOR (*(volatile uint32_t *) 0x400FF08C)
+#define GPIOC_PDDR (*(volatile uint32_t *) 0x400FF094)
+#define GPIOD_PSOR (*(volatile uint32_t *) 0x400FF0C4)
+#define GPIOD_PCOR (*(volatile uint32_t *) 0x400FF0C8)
+#define GPIOD_PTOR (*(volatile uint32_t *) 0x400FF0CC)
+#define GPIOD_PDDR (*(volatile uint32_t *) 0x400FF0D4)
+#define PORTB_PCR0 (*(volatile uint32_t *) 0x4004A000)	//SB
+#define PORTB_PCR1 (*(volatile uint32_t *) 0x4004A004)	//LAT
+#define PORTB_PCR2 (*(volatile uint32_t *) 0x4004A008)	//RST
+#define PORTC_PCR8 (*(volatile uint32_t *) 0x4004B020)	//SCK
+#define PORTC_PCR9 (*(volatile uint32_t *) 0x4004B024)	//SDA
+#define PORTA_PCR13 (*(volatile uint32_t *) 0x40049034)	//C0
+#define PORTD_PCR2 (*(volatile uint32_t *) 0x4004C008)	//C1
+#define PORTD_PCR4 (*(volatile uint32_t *) 0x4004C010)	//C2
+#define PORTD_PCR6 (*(volatile uint32_t *) 0x4004C018)	//C3
+#define PORTD_PCR7 (*(volatile uint32_t *) 0x4004C01C)	//C4
+#define PORTD_PCR5 (*(volatile uint32_t *) 0x4004C014)	//C5
+#define PORTA_PCR12 (*(volatile uint32_t *) 0x40049030)	//C6
+#define PORTA_PCR4 (*(volatile uint32_t *) 0x40049010)	//C7
 
+#define SET(x) (1 << x)
+#define CLEAR(x) (~(1 << x))
 
-void
-matrix_init();
+void	matrix_init();
+void	gpio_mode(*(volatile uint32_t *) p);
+void	sb(int);
+void	lat(int);
+void	rst(int);
+void	sck(int);
+void	sda(int);
+void	c0(int);
+void	c1(int);
+void	c2(int);
+void	c3(int);
+void	c4(int);
+void	c5(int);
+void	c6(int);
+void	c7(int);
+
 
 #endif MATRIX_H
