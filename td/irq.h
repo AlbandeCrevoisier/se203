94fbffa927ef22df2ede1110c8b16ea12dffc47b
alban <decrevoisier@enst.fr>
2016-02-03 13:41:36 +0100

    IRQ : WIP

--- td/irq.h
+++ td/irq.h
@@ -4,7 +4,9 @@
 
 #include <stdint.h>
 
-#define enable_irq() (asm ("cpsie i"))
-#define disable_irq() (asm ("cpsid i"))
+#define enable_irq() asm ("cpsie i")
+#define disable_irq() asm ("cpsid i")
+
+extern uint8_t _stack, _start;
 
 #endif // IRQ_H
